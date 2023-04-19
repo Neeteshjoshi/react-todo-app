d5a372fd83d6c3113348ee121ad291b1a079ce34
Nitesh Joshi <neetexh@gmail.com>
2023-04-19 11:30:23 +0545

    test

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,37 +1,37 @@
-import React, { useState } from "react";
-import TodoItem from "./TodoItem";
+// import React, { useState } from "react";
+// import TodoItem from "./TodoItem";
 
-const Todo = () => {
-  //Array of objects to store TODO items
-  const [todos, setTodos] = useState([]);
+// const Todo = () => {
+//   //Array of objects to store TODO items
+//   const [todos, setTodos] = useState([]);
 
-  const handleDelete = () => {
-    setTodos((prevTodos) => {
-      const newTodos = [...prevTodos];
-      newTodos.pop();
-      return newTodos;
-    });
-  };
+//   const handleDelete = () => {
+//     setTodos((prevTodos) => {
+//       const newTodos = [...prevTodos];
+//       newTodos.pop();
+//       return newTodos;
+//     });
+//   };
 
-  const handleSubmit = (inputValue) => {
-    setTodos((prevTodos) => {
-      const newTodos = [...prevTodos];
-      newTodos.push({
-        todo: inputValue,
-      });
-      return newTodos;
-    });
-  };
-  return (
-    <div>
-      <TodoItem
-        heading="Todo App"
-        todos={todos}
-        handleDeleteBtn={handleDelete}
-        handleSubmit={(inputValue) => handleSubmit(inputValue)}
-      />
-    </div>
-  );
-};
+//   const handleSubmit = (inputValue) => {
+//     setTodos((prevTodos) => {
+//       const newTodos = [...prevTodos];
+//       newTodos.push({
+//         todo: inputValue,
+//       });
+//       return newTodos;
+//     });
+//   };
+//   return (
+//     <div>
+//       <TodoItem
+//         heading="Todo App"
+//         todos={todos}
+//         handleDeleteBtn={handleDelete}
+//         handleSubmit={(inputValue) => handleSubmit(inputValue)}
+//       />
+//     </div>
+//   );
+// };
 
-export default Todo;
+// export default Todo;
